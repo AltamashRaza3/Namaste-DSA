9d1b2e0630d9b4803553d574b44ce62ae91043d7
Altamash Raza <altamashraza3.ar@gmail.com>
2025-07-27 22:57:26 +0530

    Reverse string in-place

---
+++ 03ReverseString.js
@@ -0,0 +1,15 @@
+// Reverse strings. (inplace)
+// Do not return anything, modify s-inplace instead:
+
+let s=["a","k","s","h","a","y"];
+var reverseString= function(s){
+  let length = s.length;
+  let halfLength= Math.floor(length/2);
+
+  for(let i=0;i<halfLength;i++){
+    let temp= s[i];
+    s[i]= s[length];
+    s[length]= temp;
+  }
+};
+console.log(reverseString(s));
