45a99699052dc78eed4ed2c1aaff98eb3afa1b38
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-02 13:22:06 +0530

    Convert 01Warm-Up from submodule to regular directory

---
+++ 01Warm-Up/01 ifelse.js
@@ -0,0 +1,42 @@
+
+//(1) create a function which accpets the age and tells whether a person is elogeble to vote or not
+
+
+// function elgibleToVote(age){
+//   if(age>=18 & age>0){
+//     console.log("You are eligible for vote")
+//   }
+//   else{
+//     console.log("You are not eligble for vote")
+//   }
+// }
+// elgibleToVote(-2);
+
+
+//(2) Create a function to check the age of the user and tells whether a person is eligble for vote or not and also check for valid inputs
+
+// function eligbleToVote(age){
+//   if(age<=0){
+//     console.log("Invalid Inputs")
+//   }
+//   else if (age>=18) {
+//     console.log("You are eligble for vote");
+//   } else {
+//     console.log("You are not eligble for vote")
+//   }
+// }
+
+// eligbleToVote(-1)
+
+// (3) Create a function to check if a number is Even or Odd
+
+function checkNumber(num){
+  if(num%2==0){
+    console.log("This is a even Number.")
+  }
+  else{
+    console.log("This is a Odd Number")
+  }
+}
+
+checkNumber(10);
