d079d9966a571978b6207252238f21e887b78941
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-04 14:25:41 +0530

    Adding more recusrion problems

--- 03Recursion/Recursion101.js
+++ 03Recursion/Recursion101.js
@@ -1,11 +1,29 @@
 // Print 1 to 10 with help of recursion
 // Example 1---
 
-var printNummber = function(num){
-  if(num==0) return;
-  console.log(num);
-  num = num-1;
-  printNummber(num);
+// var printNummber = function(num){
+//   if(num==0) return;
+//   console.log(num);
+//   num = num-1;
+//   printNummber(num);
+// }
+// let a=10;
+// printNummber(a);
+
+// Example 2 Print n to 1 using Recursion
+// function print(n){
+//   if(n<1) return
+//   console.log(n);
+//   print(--n);
+// }
+// print(5);
+
+// Example 3 Print 1 to n using Recursion
+function printNumber(x){
+  if(x>n) return;
+  console.log(x);
+  printNumber(++x)
 }
-let a=10;
-printNummber(a);
+let n=10;
+let x=1;
+printNumber(x);
