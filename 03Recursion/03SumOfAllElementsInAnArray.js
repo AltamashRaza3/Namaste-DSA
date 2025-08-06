7bedb64cbe9ae8bf63d96d5a18b3f89fbc08be40
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-06 12:20:01 +0530

    Sum of all the odd elements

--- 03Recursion/03SumOfAllElementsInAnArray.js
+++ 03Recursion/03SumOfAllElementsInAnArray.js
@@ -1,10 +1,21 @@
-// Find sum of the elements in the array:
+// Ex-1 Find sum of the elements in the array:
 
-let arr= [5,3,2,0,1];
+// let arr= [5,3,2,0,1];
+// function sum(n){
+//   if(n==0) return arr[0]; // Base Case
+//   return arr[n]+sum(n-1)
+// }
+
+// console.log(sum(arr.length-1))
+
+// Ex-2  Find the sum of all odd numbers in the array
+let arr = [5,3,2,0,1]; // Skips those number which are even
 
 function sum(n){
-  if(n==0) return arr[0]; // Base Case
-  return arr[n]+sum(n-1)
+  let isOdd = ((arr[n] %2 )!== 0);
+  if( n== 0){
+    return isOdd ? arr[n] : 0;
+  }
+  return ((isOdd ? arr[n]:0) + sum(n-1))
 }
-
-console.log(sum(arr.length-1))
+console.log(sum(arr.length-1));
