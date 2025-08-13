7e0fcc2523b8bb7014b7a8eaaa7595e1201c674e
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-13 23:57:26 +0530

    Bubble sort

---
+++ 04Searching&Sorting/03BubbleSort.js
@@ -0,0 +1,21 @@
+// Bubble sorting : Check adjacent element are compared and swapped to move 
+// larger elements to the ends of the list
+
+let arr= [5,4,9,15,1,0];
+
+let n= arr.length;
+
+function bubbleSort(arr){
+for(let i=0;i<n-1;i++){
+  for(let j=0;j<n-1-i;j++){
+    if(arr[j]>arr[j+1]){
+      let temp = arr[j];
+      arr[j]= arr[j+1];
+      arr[j+1]= temp;
+    }
+  }
+}
+return arr;
+}
+let result = bubbleSort(arr);
+console.log(result);
