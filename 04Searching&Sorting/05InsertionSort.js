2820c728f907fabaacffa11893dcfac4a3194f5c
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-16 23:06:42 +0530

    InsertionSort Algorithm

---
+++ 04Searching&Sorting/05InsertionSort.js
@@ -0,0 +1,18 @@
+// Insertion sort technique where each element is inserted at the correct position
+// in the sorted part of the array.
+
+let arr= [7,1,5,12,-10,0,4,3,2];
+
+function insertionSort(arr){
+  let n = arr.length;
+  for(let i=1;i<n-1;i++){
+    let curr = a[i];
+    let perv = curr-1;
+    while(arr[perv] > curr && prev >=0){
+      let temp= i;
+      
+    }
+  }
+  
+  
+}
