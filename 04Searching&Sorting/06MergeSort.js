3e0376b5ed2f00b3ed0cde23c55155ff0c4ae9ce
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-19 12:55:03 +0530

    Changing Input values

--- 04Searching&Sorting/06MergeSort.js
+++ 04Searching&Sorting/06MergeSort.js
@@ -1,15 +1,24 @@
 // Merge two sorted Arrays:
 let a1= [2,4,8,9];
+let m =a1.length;
 let a2= [1,3,5,7];
+let n=a2.length;
 
-let m =3;
-let n=3;
-let a1copy= a1;
+function mergeArray(a1,m,a2,n){
+let a1copy= a1.slice(0,m); // [2,4,8,9]
+  let p1= 0;
+  let p2= 0;
 
-function mergeTwoSortedArray(){
-
-
-for(let i=0;i<m+n;i++){
-  if(a2>=n)
+  for(let i=0; i<m+n; i++){
+    if(p2>=n || (p1<m && a1copy[p1] < a2[p2])){
+      a1[i] = a1copy[p1];
+      p1++;
+    }
+    else{
+      a1[i]= a2[p2];
+      p2++
+    }
+  }
+  return a1;
 }
-}
+console.log(mergeArray(a1,m,a2,n));
