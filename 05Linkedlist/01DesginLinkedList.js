096d4bdd307010ddd1861d7f8c1e2bc83e8ebe35
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-25 23:48:24 +0530

    Add element to the tail

--- 05Linkedlist/01DesginLinkedList.js
+++ 05Linkedlist/01DesginLinkedList.js
@@ -17,20 +17,27 @@ function newLinkedList(){
 function addAtHead(val){
   let newNode= new Node(val);
   newNode.next = this.head;
-  this.head = new Node;
+  this.head = newNode;
   this.size ++ ;
 }
 
 // Add new element to the tail
   function addToTail(val){
+    let newNode = new Node(val);
+    // Corner-case --> if linkedlist is empty then
+    if(this.head==null){
+      this.head= newNode;
+    }
     // reach to the last element
+    else{
     let curr= this.head;
     while(curr.next != null){
       curr= curr.next;
     }
-    let newNode = new Node(val);
     curr.next = newNode;
   }
+  this.size++;
+}
 // Add new element at particular index
 
 // Deleting a element at the paticular index
