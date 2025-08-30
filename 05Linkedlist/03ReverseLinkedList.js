f98ceca9442e39b9cf867c837ce47b395565177a
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-30 15:01:31 +0530

    Reverse a linkedList

---
+++ 05Linkedlist/03ReverseLinkedList.js
@@ -0,0 +1,14 @@
+// Reverse a linkedList: 
+
+let reverseList = function (head){
+  let prev= null;
+  let curr = head;
+  while(curr){
+    let temp= curr.next;
+    curr.next= prev;
+    prev= curr;
+    curr = temp;
+  }
+  head=prev;
+  return head;
+}
