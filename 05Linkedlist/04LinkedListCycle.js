79d0d7496a34c7684d7aee710c6c2256b5af6729
Altamash Raza <altamashraza3.ar@gmail.com>
2025-09-11 23:49:24 +0530

    update cyclic linkedlist code

--- 05Linkedlist/04LinkedListCycle.js
+++ 05Linkedlist/04LinkedListCycle.js
@@ -1,14 +1,15 @@
 // How to find this linkedlist have cycle or not   LeetCode- 141
 
-function isCycle(head){
-  let seenNodes = head;
+function hasCycle(head) {
+  const seenNodes = new Set();
   let curr = head;
-  while(curr !=null){
-    if(seenNodes.has(curr)){
+  while (curr !== null) {
+    if (seenNodes.has(curr)) {
       return true;
-    } 
+    }
     seenNodes.add(curr);
-    curr =curr.next;
+    curr = curr.next;
   }
   return false;
 }
+
